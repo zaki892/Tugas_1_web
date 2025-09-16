5d6393edffc02caa44f6345d0d9a514a9bb97667
zaki892 <63931767+zaki892@users.noreply.github.com>
2025-09-17 03:32:35 +0700

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '',
+}
+
+export default nextConfig
